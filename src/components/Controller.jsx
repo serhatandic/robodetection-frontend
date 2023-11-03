fa05bbc6cb1f356b1835ed64f630ec433cc869fe
serhat andic <serhatandic42@gmail.com>
2023-11-03 19:48:16 +0300

    add details section to the left of the controller

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -91,22 +91,54 @@ const Controller = () => {
 		console.log(currentPressedButton);
 	}, [currentPressedButton]);
 	return (
-		<div className='border-2 w-full h-1/2 border-black flex flex-col justify-center'>
-			<div className='flex justify-center items-center text-xl'>
-				<IconButton ref={upRef}>
-					<KeyboardArrowUpIcon />
-				</IconButton>
+		<div className='border-2 w-full h-1/4 border-black flex justify-between'>
+			<div className='overflow-auto h-full w-full'>
+				<div>
+					<table className='ml-2 mt-2'>
+						<thead>
+							<tr>
+								<th className='p-2'>Speed</th>
+								<th className='p-2'>Direction</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td className='p-2'>0.0</td>
+								<td className='p-2'>0.0</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div className='overflow-auto'>
+					{/*activity log*/}
+					<h2 className='ml-4 mt-2 font-bold'>Activity Log:</h2>
+					<div className='ml-4'>
+						<p>Dog started to walk {Math.random() * 1000000}</p>
+						<p>Direction changed {Math.random() * 1000000}</p>
+						<p>Other log {Math.random() * 1000000}</p>
+						<p>Some other log {Math.random() * 1000000}</p>
+						<p>Loglog {Math.random() * 1000000}</p>
+						<p>Logologoglgogolg {Math.random() * 1000000}</p>
+					</div>
+				</div>
 			</div>
-			<div className='flex justify-center items-center'>
-				<IconButton ref={leftRef}>
-					<KeyboardArrowLeftIcon />
-				</IconButton>
-				<IconButton ref={downRef}>
-					<KeyboardArrowDownIcon />
-				</IconButton>
-				<IconButton ref={rightRef}>
-					<KeyboardArrowRightIcon />
-				</IconButton>
+			<div className='border-l-2 border-black w-1/3 h-full flex flex-col justify-center'>
+				<div className='flex justify-center items-center text-xl'>
+					<IconButton ref={upRef}>
+						<KeyboardArrowUpIcon />
+					</IconButton>
+				</div>
+				<div className='flex justify-center items-center'>
+					<IconButton ref={leftRef}>
+						<KeyboardArrowLeftIcon />
+					</IconButton>
+					<IconButton ref={downRef}>
+						<KeyboardArrowDownIcon />
+					</IconButton>
+					<IconButton ref={rightRef}>
+						<KeyboardArrowRightIcon />
+					</IconButton>
+				</div>
 			</div>
 		</div>
 	);
