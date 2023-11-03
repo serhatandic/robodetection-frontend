17da142f5d5393454dea522a730495d466f86c11
serhat andic <serhatandic42@gmail.com>
2023-11-03 19:13:36 +0300

    switch to a better table

--- src/components/TrackablesList.jsx
+++ src/components/TrackablesList.jsx
@@ -1,69 +1,44 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import { DataGrid } from '@mui/x-data-grid';
 
-const TrackablesList = () => {
-	function createData(id, name, calories, fat, carbs, protein) {
-		return { id, name, calories, fat, carbs, protein };
-	}
-	const rows = [
-		createData(1, 'Barış Sarper TEZCAN', 2200, 6.0, 24, 5000),
-		createData(2, 'Barış PC', 0, 0, 0, 0),
-		createData(3, 'Hikmet Rasih Türkan', 2300, 16.0, 24, 2500),
-		createData(4, 'Hikmet PC', 0, 0, 0, 0),
-		createData(5, 'Gingerbread', 356, 16.0, 49, 3.9),
-		createData(6, 'Cupcake', 305, 3.7, 67, 4.3),
-		createData(7, 'Cupcake', 305, 3.7, 67, 4.3),
-		createData(8, 'Cupcake', 305, 3.7, 67, 4.3),
-		createData(9, 'Cupcake', 305, 3.7, 67, 4.3),
-	];
+const columns = [
+	{ field: 'id', headerName: 'ID', width: 70 },
+	{ field: 'firstName', headerName: 'First name', width: 130 },
+	{ field: 'lastName', headerName: 'Last name', width: 130 },
+	{
+		field: 'age',
+		headerName: 'Age',
+		type: 'number',
+		width: 90,
+	},
+	{
+		field: 'fullName',
+		headerName: 'Full name',
+		description: 'This column has a value getter and is not sortable.',
+		sortable: false,
+		width: 160,
+		valueGetter: (params) =>
+			`${params.row.firstName || ''} ${params.row.lastName || ''}`,
+	},
+];
+
+const rows = [
+	{ id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+	{ id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+	{ id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+	{ id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+	{ id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+	{ id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+	{ id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+	{ id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+	{ id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+function TrackablesList() {
 	return (
-		<div className='border-2 w-full h-full border-black overflow-auto'>
-			<TableContainer component={Paper}>
-				<Table sx={{ minWidth: 650 }} aria-label='simple table'>
-					<TableHead>
-						<TableRow>
-							<TableCell>Dessert (100g serving)</TableCell>
-							<TableCell align='right'>Calories</TableCell>
-							<TableCell align='right'>Fat&nbsp;(g)</TableCell>
-							<TableCell align='right'>Carbs&nbsp;(g)</TableCell>
-							<TableCell align='right'>
-								Protein&nbsp;(g)
-							</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{rows.map((row) => (
-							<TableRow
-								key={row.id}
-								sx={{
-									'&:last-child td, &:last-child th': {
-										border: 0,
-									},
-								}}
-							>
-								<TableCell component='th' scope='row'>
-									{row.name}
-								</TableCell>
-								<TableCell align='right'>
-									{row.calories}
-								</TableCell>
-								<TableCell align='right'>{row.fat}</TableCell>
-								<TableCell align='right'>{row.carbs}</TableCell>
-								<TableCell align='right'>
-									{row.protein}
-								</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</TableContainer>
+		<div className='h-full overflow-auto'>
+			<DataGrid rows={rows} columns={columns} />
 		</div>
 	);
-};
+}
 
 export default TrackablesList;
